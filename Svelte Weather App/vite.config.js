ef5540294157204889366a0b392f75098dbb4329
tamakachi <kenttechnical@gmail.com>
2023-08-03 22:04:10 +0200

    Github pages compatibility

--- Svelte Weather App/vite.config.js
+++ Svelte Weather App/vite.config.js
@@ -4,4 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+
+ // Added this to make it work on github pages
+  base: "/Stormscout/"
 })
