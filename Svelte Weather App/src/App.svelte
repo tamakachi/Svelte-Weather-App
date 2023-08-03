9f0de6be6eafa6489ad6b78615ca11da8eb478fc
tamakachi <kenttechnical@gmail.com>
2023-08-04 01:33:55 +0200

    Reverting

--- Svelte Weather App/src/App.svelte
+++ Svelte Weather App/src/App.svelte
@@ -1,22 +1,7 @@
 <script>
 // Developed by Kent Van Heerden
 
-
 import Weather from "./lib/Weather.svelte";
-import { onMount } from "svelte";
-
-
-function zoomOut() {
-  var zoom = window.document.body.style.transform;
-  if (zoom === undefined) {
-    zoom = "scale(1)";
-  }
-  window.document.body.style.transform = "scale(0)";
-}
-
-onMount(() => {
-    zoomOut()
-});
 
 </script>
 
