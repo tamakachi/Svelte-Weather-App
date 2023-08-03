0bfd22ab44c582b09276cc06089e44d4d5c0a250
tamakachi <kenttechnical@gmail.com>
2023-08-04 01:30:44 +0200

    Trying to make the site zoom out by default

--- Svelte Weather App/src/App.svelte
+++ Svelte Weather App/src/App.svelte
@@ -1,7 +1,22 @@
 <script>
-import Weather from "./lib/Weather.svelte";
-
 // Developed by Kent Van Heerden
+
+
+import Weather from "./lib/Weather.svelte";
+import { onMount } from "svelte";
+
+
+function zoomOut() {
+  var zoom = window.document.body.style.transform;
+  if (zoom === undefined) {
+    zoom = "scale(1)";
+  }
+  window.document.body.style.transform = "scale(0)";
+}
+
+onMount(() => {
+    zoomOut()
+});
 
 </script>
 
